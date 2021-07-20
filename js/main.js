780e6c56b2895ebad21ef7b6824a2b2d4feb1790
Marco <sonni46marcosenpai@gmail.com>
2021-07-20 13:15:12 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// 1. chiedi all’utente il cognome
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// 3. stampa la lista ordinata alfabeticamente
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+
+var utente = prompt("scrivi il tuo cognome")
+
+var array = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"]
